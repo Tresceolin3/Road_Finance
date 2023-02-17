869b17930af753c321409928b23961ed70adfe94
Gabriel Ceolin <gabrieltresceolin@gmail.com>
2023-02-17 08:47:29 -0400

    adapter de movimentacao

--- app/src/main/java/com/example/roadfinance/activity/activity/PrincipalActivity.java
+++ app/src/main/java/com/example/roadfinance/activity/activity/PrincipalActivity.java
@@ -13,12 +13,13 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.roadfinance.R;
+import com.example.roadfinance.activity.adapter.AdapterMovimentacao;
 import com.example.roadfinance.activity.config.ConfiguraçaoFirebase;
 import com.example.roadfinance.activity.helper.Base64Custom;
+import com.example.roadfinance.activity.model.Movimentacao;
 import com.example.roadfinance.activity.model.Usuario;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -30,6 +31,8 @@ import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrincipalActivity extends AppCompatActivity {
     private MaterialCalendarView calendarView;
@@ -42,6 +45,13 @@ public class PrincipalActivity extends AppCompatActivity {
     private DatabaseReference firebaseRef = ConfiguraçaoFirebase.getFirebaseDatabase();
     private DatabaseReference usuarioRef;
     private ValueEventListener valueEventListenerUsuario;
+    private ValueEventListener valueEventListenerMovimentacoes;
+
+    private AdapterMovimentacao adapterMovimentacao;
+    private List<Movimentacao> movimentacoes = new ArrayList<>();
+    private DatabaseReference movimentacaoRef;
+
+    private String mesAnoSelecionado;
 
     private RecyclerView recyclerView;
 
@@ -49,6 +59,7 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Road Finance");
         setSupportActionBar(toolbar);
@@ -62,19 +73,38 @@ public class PrincipalActivity extends AppCompatActivity {
         recyclerView = findViewById(R.id.recyclerMovimento);
 
         //configurar adapter
-
+        adapterMovimentacao = new AdapterMovimentacao(movimentacoes, this);
         //configurar Recycleview
-        RecyclerView.LayoutManager  layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager( layoutManager );
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
-       // recyclerView.setAdapter();
-
+        recyclerView.setAdapter(adapterMovimentacao);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        recuperarResumo();
+    public void recuperarMovimentacoes() {
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64(emailUsuario);
+        movimentacaoRef = firebaseRef.child("movimentacao")
+                .child(idUsuario)
+                .child(mesAnoSelecionado);
+
+        valueEventListenerMovimentacoes = movimentacaoRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                movimentacoes.clear();
+                for (DataSnapshot dados : dataSnapshot.getChildren()) {
+                    Movimentacao movimentacao = dados.getValue(Movimentacao.class);
+                    movimentacoes.add(movimentacao);
+                }
+                adapterMovimentacao.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public void recuperarResumo() {
@@ -126,6 +156,41 @@ public class PrincipalActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void configuraCalenderView() {
+        CharSequence meses[] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho",
+                "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        calendarView.setTitleMonths(meses);
+        CalendarDay dataAtual = calendarView.getCurrentDate();
+        String mesSelecionado = String.format("%02d", (dataAtual.getMonth() + 1));
+        mesAnoSelecionado = String.valueOf(mesSelecionado + "" + dataAtual.getYear());
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+                String mesSelecionao = String.format("%02d", (date.getMonth() + 1));
+                mesAnoSelecionado = String.valueOf(mesSelecionao + "" + date.getYear());
+                movimentacaoRef.removeEventListener(valueEventListenerMovimentacoes);
+                recuperarMovimentacoes();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recuperarResumo();
+        recuperarMovimentacoes();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Evento", "evento foi removido!");
+        usuarioRef.removeEventListener(valueEventListenerUsuario);
+        movimentacaoRef.removeEventListener(valueEventListenerMovimentacoes);
+    }
+
     public void adicionarReceita(View view) {
         startActivity(new Intent(this, ReceitasActivity.class));
 
@@ -141,25 +206,5 @@ public class PrincipalActivity extends AppCompatActivity {
 
     public void adicionarEmpresa(View view) {
         startActivity(new Intent(this, CadastarEmpresaActivity.class));
-    }
-
-    public void configuraCalenderView() {
-        CharSequence meses[] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho",
-                "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
-        calendarView.setTitleMonths(meses);
-
-        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
-            @Override
-            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
-
-            }
-        });
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i("Evento", "evento foi removido!");
-        usuarioRef.removeEventListener(valueEventListenerUsuario);
     }
 }
